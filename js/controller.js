1fa7f936ea3e53e0573d4441133fbe38e0b7f087
nufa flufa <nofargabso@gmail.com>
2021-02-10 19:17:29 +0200

    start dev tools and grid

--- js/controller.js
+++ js/controller.js
@@ -11,38 +11,49 @@ function onInit() {
     renderImgGallery()
 }
 
-function onChangeCanvas(ev){
+function onChangeCanvas(ev) {
     ev.preventDefault();
     renderCanvas();
 }
 
 function renderCanvas() {
     gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height);
-    if(!gMeme.selectedImgId) return;
+    if (!gMeme.selectedImgId) return;
     drawImg();
     var currText = document.querySelector('input[name=txt1]').value;
     gMeme.lines[0].txt = currText;
     setTimeout(drawText, 100);
 }
 
-function onDrawImg() {
-    drawImg();
-}
 
-// function onDrawText() {
-//     drawText();
-// }
-
-function renderImgGallery(){
+function renderImgGallery() {
     var strHTMLs = gImgs.map((img) => {
         return `<img src=${img.url} data-number=${img.id} class="image-item" onclick="onChangeImg(this)">`
     })
     document.querySelector('.imgs-gallery').innerHTML = strHTMLs.join('');
 }
 
-function onChangeImg(el){
+function onChangeImg(el) {
     var newImgId = parseInt(el.dataset.number)
     gMeme.selectedImgId = newImgId;
+    renderCanvas();
+}
+
+function onChangeFontSize(sign) {
+    if (gMeme.lines[gMeme.selectedLineIdx].size < 20) return;
+    if (gMeme.lines[gMeme.selectedLineIdx].size > 300) return;
+
+    if (sign === '+') gMeme.lines[gMeme.selectedLineIdx].size += 10
+    else if (sign === '-') gMeme.lines[gMeme.selectedLineIdx].size -= 10
+
+    renderCanvas();
+}
+
+function onMoveText(direction) {
+    
+    if (direction === 'up') gMeme.pos.y -= 20;
+    else if (direction === 'down') gMeme.pos.y += 20
+
     renderCanvas();
 }
 
--- js/service/meme-service.js
+++ js/service/meme-service.js
@@ -3,10 +3,17 @@
 var gMeme = {
     selectedImgId: 0,
     selectedLineIdx: 0,
+    pos: { x: 250, y: 100 },
     lines: [
         {
             txt: 'I never eat Falafel',
             size: 20,
+            align: 'left',
+            color: 'white'
+        },
+        {
+            txt: 'I never eat Falafel',
+            size: 100,
             align: 'left',
             color: 'white'
         },
@@ -20,19 +27,23 @@ function drawImg() {
         return gMeme.selectedImgId === img.id
     })
     img.src = selectedImg.url
-    
+
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
     }
 }
 
-function drawText(){
+function drawText() {
     var txt = gMeme.lines[gMeme.selectedLineIdx].txt
+    var size = gMeme.lines[gMeme.selectedLineIdx].size
+    console.log(size)
     gCtx.lineWidth = 2;
     gCtx.strokeStyle = gMeme.color;
-    gCtx.fillStyle =  'white';
-    gCtx.font = '50px Impact';
+    gCtx.fillStyle = 'white';
+    gCtx.font = `${size}px Impact`;
     gCtx.textAlign = 'center'
-    gCtx.fillText(txt,250, 100)
-    gCtx.strokeText(txt, 250, 100)
+    gCtx.fillText(txt, gMeme.pos.x, gMeme.pos.y)
+    gCtx.strokeText(txt, gMeme.pos.x, gMeme.pos.y)
 }
+
+// x = gElCanvas.width / 2, y = gElCanvas.height / (gElCanvas.height / 100)
