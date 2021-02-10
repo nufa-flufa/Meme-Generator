d97b5b1e3bf856905b0a26dbbf3c15f92e8d8143
nufa flufa <nofargabso@gmail.com>
2021-02-10 12:43:18 +0200

    change img url settings

--- js/service/img-bank.js
+++ js/service/img-bank.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var gImgs = [
-    { id: 1, url: '/meme-imgs/1.jpg', keywords: [] },
-    { id: 2, url: '/meme-imgs/2.jpg', keywords: [] },
-    { id: 3, url: '/meme-imgs/3.jpg', keywords: [] },
+    { id: 1, url: '../meme-imgs/1.jpg', keywords: [] },
+    { id: 2, url: '../meme-imgs/2.jpg', keywords: [] },
+    { id: 3, url: '../meme-imgs/3.jpg', keywords: [] },
 ]
