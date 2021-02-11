3e74f843474e5598820d73056f384118c95d5483
nufa flufa <nofargabso@gmail.com>
2021-02-11 18:11:49 +0200

    adding buttons, style page and fixing bugs

--- js/controller.js
+++ js/controller.js
@@ -20,8 +20,10 @@ function renderCanvas() {
     gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height);
     if (!gMeme.selectedImgId) return;
     drawImg();
-    var currText = document.querySelector('input[name=txt1]').value;
-    gMeme.lines[0].txt = currText;
+
+    var currInputTxtVal = (gMeme.selectedLineIdx === 1) ? 'input[name=txt2]' : 'input[name=txt1]';
+    var currText = document.querySelector(currInputTxtVal).value;
+    gMeme.lines[gMeme.selectedLineIdx].txt = currText;
     setTimeout(drawText, 100);
 }
 
@@ -39,6 +41,25 @@ function onChangeImg(el) {
     renderCanvas();
 }
 
+function onRemoveText() {
+    removeLine()
+    renderCanvas();
+}
+
+function onAddLine() {
+    if(gMeme.lines.length === 2) return;
+    var elForm = document.querySelector('form')
+    let newTxtLine = document.createElement('div')
+    var strHTML = `<input class="txt-input" type="text" placeholder="Write your text here" name="txt2">`
+    elForm.appendChild(newTxtLine).innerHTML = strHTML
+    gMeme.selectedLineIdx += 1;
+    // console.log('',gMeme.selectedLineIdx)
+    // elForm.appendChild(newTxtLine).style.gridRow= '2/3';
+    // elForm.appendChild(newTxtLine).style.gridColumn= '8/9';
+    addLineObject();
+}
+
+
 function onChangeFontSize(sign) {
     if (gMeme.lines[gMeme.selectedLineIdx].size < 20) return;
     if (gMeme.lines[gMeme.selectedLineIdx].size > 300) return;
@@ -50,21 +71,34 @@ function onChangeFontSize(sign) {
 }
 
 function onMoveText(direction) {
-    
-    if (direction === 'up') gMeme.pos.y -= 20;
-    else if (direction === 'down') gMeme.pos.y += 20
+    if (direction === 'up') gMeme.lines[gMeme.selectedLineIdx].pos.y -= 20;
+    else if (direction === 'down') gMeme.lines[gMeme.selectedLineIdx].pos.y += 20;
 
     renderCanvas();
 }
 
+function onChangeLineFocus() {
+    if (gMeme.selectedLineIdx === 1) {
+        gMeme.selectedLineIdx = 0;
+        document.querySelector(`input[name=txt1]`).style.border = '3px solid black';
+        document.querySelector(`input[name=txt2]`).style.border = 'none';
+    }
+    else {
+        gMeme.selectedLineIdx = 1;
+        document.querySelector(`input[name=txt2]`).style.border = '3px solid black';
+        document.querySelector(`input[name=txt1]`).style.border = 'none';
+
+    }
+}
 function addMouseListener() {
-    gElCanvas.addEventListener('mousedown', getPos)
+    window.addEventListener('mousedown', getPos)
 }
 
 function getPos(ev) {
     var pos = {
-        x: ev.offsetX,
-        y: ev.offsetY
+        x: ev.clientX,
+        y: ev.clientY
     }
-    console.log(pos)
+    // if (pos.y > 150 && pos.y < 205 && pos.x > 805 && pos.x < 1165) gMeme.selectedLineIdx = 0;
+        console.log(pos)
 }
--- js/service/meme-service.js
+++ js/service/meme-service.js
@@ -3,19 +3,13 @@
 var gMeme = {
     selectedImgId: 0,
     selectedLineIdx: 0,
-    pos: { x: 250, y: 100 },
     lines: [
         {
-            txt: 'I never eat Falafel',
-            size: 20,
+            txt: '',
+            size: 40,
             align: 'left',
-            color: 'white'
-        },
-        {
-            txt: 'I never eat Falafel',
-            size: 100,
-            align: 'left',
-            color: 'white'
+            color: 'white',
+            pos: { x: 250, y: 100 }
         },
     ],
 }
@@ -34,16 +28,47 @@ function drawImg() {
 }
 
 function drawText() {
-    var txt = gMeme.lines[gMeme.selectedLineIdx].txt
-    var size = gMeme.lines[gMeme.selectedLineIdx].size
-    console.log(size)
-    gCtx.lineWidth = 2;
-    gCtx.strokeStyle = gMeme.color;
-    gCtx.fillStyle = 'white';
-    gCtx.font = `${size}px Impact`;
-    gCtx.textAlign = 'center'
-    gCtx.fillText(txt, gMeme.pos.x, gMeme.pos.y)
-    gCtx.strokeText(txt, gMeme.pos.x, gMeme.pos.y)
+    gMeme.lines.map((line) => {
+        var posX = line.pos.x
+        var posY = line.pos.y
+        gCtx.lineWidth = 2;
+        gCtx.strokeStyle = 'black';
+        gCtx.fillStyle = 'white';
+        gCtx.font = `${line.size}px Impact`;
+        gCtx.textAlign = 'center'
+        gCtx.fillText(line.txt, posX, posY)
+        gCtx.strokeText(line.txt, posX, posY)
+    })
+
 }
 
+function getPos(ev) {
+    var pos = {
+        x: ev.offsetX,
+        y: ev.offsetY
+    }
+    // console.log(pos)
+}
+
+function removeLine() {
+    console.log('txt', gMeme.lines[gMeme.selectedLineIdx.txt])
+    gMeme.lines[gMeme.selectedLineIdx].txt = '';
+    console.log('selectedLineIdx:', gMeme.selectedLineIdx)
+    document.querySelector('input[name=txt1]').value = '';
+    console.log('txt after', gMeme.lines[gMeme.selectedLineIdx].txt)
+}
+
+function addLineObject() {
+    // console.log(gMeme)
+    if (gMeme.lines.length === 2) return;
+    var newLine = {
+        txt: '',
+        size: 40,
+        align: 'left',
+        color: 'white',
+        pos: (gMeme.lines.length === 1) ? { x: 250, y: 400 } : { x: 250, y: 250 },
+    }
+    gMeme.lines.push(newLine);
+    console.log(gMeme)
+}
 // x = gElCanvas.width / 2, y = gElCanvas.height / (gElCanvas.height / 100)
